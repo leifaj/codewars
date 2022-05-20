b6f035c6febbce8fff41666d3d2b3e25df3d46a8
Leianne Fajutagana <leiannefaj@gmail.com>
2022-05-20 11:11:31 -0700

    Add: 5kyu folder and WeightForWeight

---
+++ 5kyu/WeightForWeight/index.js
@@ -0,0 +1,18 @@
+function orderWeight(str) {
+  nums = str.split(" ");
+  nums.sort(compareWeights);
+
+  function compareWeights(a, b) {
+    let weightA = a.split("").reduce((acc, curr) => Number(acc) + Number(curr), 0);
+    let weightB = b.split("").reduce((acc, curr) => Number(acc) + Number(curr), 0);
+    
+    if (a === b) return 0;
+    if (weightA > weightB) return 1;
+    if (weightA < weightB) return -1;
+    if (weightA === weightB) {
+      return String(a) > String(b) ? 1 : -1;
+    }
+  }
+  
+  return nums.join(" ");
+}
